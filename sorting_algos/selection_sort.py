4358f13309015d741a954d0f9edf0968a9272e94
desai-mohan <desaimohanreddy09@gmail.com>
2026-04-03 06:58:37 +0530

    added comments

--- sorting_algos/selection_sort.py
+++ sorting_algos/selection_sort.py
@@ -1,11 +1,11 @@
 def selection_sort(arr):  
     n=len(arr)
     for i in range(n):
-        min_index=i 
+        min_index=i #assume first element is minimum
         for j in range(i+1,n):
-            if arr[j]<arr[min_index]:
+            if arr[j]<arr[min_index]:#check if there is a smaller element than current min, if update min_index
                 min_index=j 
-        arr[i],arr[min_index]=arr[min_index],arr[i]
+        arr[i],arr[min_index]=arr[min_index],arr[i]#swap element with index i with min_index element
     return arr 
 
 array=[64, 25, 12, 22, 11] 
